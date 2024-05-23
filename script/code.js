cf0d1a36fcee6cfeb5c9bb53d6f79a8e9921fc9e
amyj0hn <amyj62450@gmail.com>
2024-05-23 11:06:55 +0200

    Finished the task

---
+++ script/code.js
@@ -0,0 +1,17 @@
+function greet(name){
+
+    console.log(`Hello ${name}`)
+}greet("Amy")
+
+function multiply(num1, num2){
+
+console.log(num1 * num2)
+
+}multiply(2, 6)
+
+
+function /*function name*/ tableOfFive(number /*parameter */){
+
+    return number * 5 
+}
+console.log(tableOfFive(5))
